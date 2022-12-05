6fabcd9d48764884c50ac13dc4b1948455939064
“awslinux” <mbmkrishna4602@gmail.com>
2022-12-05 17:40:31 +0530

    my docker commit

---
+++ text1.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+
+     stages {
+         stage('Add Folder')  {
+            steps {
+            sh 'mkdir  shivani-folder'
+            sh 'ls -ltrh'
+            sh ' echo "I am working"'
+            }
+              
+         
+     
+
+
