a607f5e98d5f3b017afd873c47d2123b2073637c
“awslinux” <mbmkrishna4602@gmail.com>
2022-12-05 18:23:08 +0530

    my last commir

---
+++ text2.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+    stages{
+        stage('printing working directory'){
+            steps{
+                sh 'pwd'
+            }
+        }
+    stage ('clone repo'){
+        steps {
+       withCredentials([usernamePassword(credentialsId: '509eda60-1194-4ccf-b8e7-2e5fb10e8a56', passwordVariable: 'shivani@1234', usernameVariable: 'shivani')]) {
+
+}
+
+
+
+}
+            echo "repo cloned"
+            sh 'ls -ltrh'
+    }
+    }
+    }
+    stage ('build dockerfile'){
+        steps {
+            sh "docker build -t image14 ."
+        }
+    }
+    stage ('docker images'){
+    steps {
+        sh "docker images"
+    }
+}
+    stage ('docker run the image'){
+    steps {
+        sh "docker run -d -p 8080:8086 image14 "
+    }
+}
+    }
+}
+
+
