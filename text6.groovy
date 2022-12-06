c9ff7632f493e25ced0d15b351d86c5d4012a568
“awslinux” <mbmkrishna4602@gmail.com>
2022-12-06 20:27:40 +0530

    my commit3

---
+++ text6.groovy
@@ -0,0 +1,32 @@
+pipeline  {
+    agent any
+    stages {
+        stage('printing working directory') {
+            steps {
+                sh 'pwd'
+            }
+        }
+        stage ('clone repo') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: '509eda60-1194-4ccf-b8e7-2e5fb10e8a56', passwordVariable: 'password', usernameVariable: 'username')]) {
+                }
+           echo "repo cloned"
+           sh  'ls -ltrh'
+            }
+        }
+    }
+       stage ('build dockerfile') {
+        steps {
+            sh "docker build -t image1"
+        }
+        
+       }
+
+}
+            
+
+    
+
+            
+        
+    
